acb10f359ec722697c54a9986259d2a540818949
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-06-30 14:37:45 -0500

    apiUpdate

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
-const connect = async () =>{
-    try{
-        await mongoose.connect(process.env.MONGO);
-        console.log("MongoDB Connected");
-    } catch(err){
-        throw err;
-    }
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
 };
 
 mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB Disconnected");
+  console.log("mongoDB disconnected!");
 });
 
-//middleware
+//middlewares
+app.use(cors())
+app.use(cookieParser())
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -30,19 +34,18 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-//customized error
 app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || "Something went wrong";
-    return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack,
-    });
-})
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
-app.listen(8800, ()=>{
-    connect();
-    console.log("Connected to backend!");
-})
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,16 +1,13 @@
 import express from 'express';
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.post('/', createHotel);
-
-router.put('/:id', updateHotel);
-
+router.post('/', verifyAdmin, createHotel);
+router.put('/:id', verifyAdmin, updateHotel);
 router.delete('/:id', deleteHotel);
-
 router.get('/:id', getHotel);
-
 router.get('/', getHotels);
 
 export default router
